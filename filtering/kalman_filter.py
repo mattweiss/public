f0d3457d1180041ba9a790e49416ae4cdbb4b269
Matt <mlweiss@wpi.edu>
2019-08-20 14:43:30 -0400

    - Tightened up filter() code, no changes in implementation

--- filtering/kalman_filter.py
+++ filtering/kalman_filter.py
@@ -25,35 +25,36 @@ class KalmanFilter(AbstractFilter):
 
         with tf.Session() as sess:
         
-            self._x_hat_pri, self._x_hat_post, self._P_hat_pri, self._P_hat_post, self._kf_ctr = sess.run( tf.scan(self._kfScan,
-                                                                                                                   self._z,
-                                                                                                                   initializer = [ self._x0, self._x0, self._P0, self._P0, tf.constant(0) ], name='kfScan'), feed_dict={self._z:z})
+            self._x_hat_pri, self._x_hat_post,\
+            self._z_hat_pri, self._z_hat_post,\
+            self._P_hat_pri, self._P_hat_post, self._kf_ctr = sess.run( tf.scan(self._kfScan,
+                                                                                self._z,
+                                                                                initializer = [ self._x0, self._x0,
+                                                                                                self._z0, self._z0,
+                                                                                                self._P0, self._P0,
+                                                                                                tf.constant(0) ], name='kfScan'), feed_dict={self._z:z})
 
-            self._x_hat_pri = np.squeeze(self._x_hat_pri)
-            self._x_hat_post = np.squeeze(self._x_hat_post)
-            
-            self._z_hat_pri = sess.run(tf.matmul(self._x_hat_pri, self._H, name='z_hat_pri', transpose_b=True))
-            self._z_hat_post = sess.run(tf.matmul(self._x_hat_post, self._H, name='z_hat_post', transpose_b=True))
+        return { 'x_hat_pri':np.squeeze(self._x_hat_pri), 'x_hat_post':np.squeeze(self._x_hat_post),
+                 'z_hat_pri':np.squeeze(self._z_hat_pri), 'z_hat_post':np.squeeze(self._z_hat_post),
+                 'P_hat_pri':self._P_hat_pri, 'P_hat_post':self._P_hat_post,
+                 'z':self._z }
 
-            return { 'x_hat_pri':self._x_hat_pri, 'x_hat_post':self._x_hat_post,
-                     'P_hat_pri':self._P_hat_pri, 'P_hat_post':self._P_hat_post,
-                     'z_hat_pri':self._z_hat_pri, 'z_hat_post':self._z_hat_post, 'z':self._z }
+################################################################################
 
+        
 ################################################################################
 
     def _kfScan(self, state, z):
 
         """ This is where the acutal Kalman Filter is implemented. """
 
-        x_pri, x_post, P_pri, P_post, self._kf_ctr = state
+        x_pri, x_post, z_pri, z_post, P_pri, P_post, self._kf_ctr = state
 
         # reset state estimate each minibatch
-        x_pri, x_post, P_pri, P_post, self._kf_ctr = tf.cond( tf.less( self._kf_ctr, self._n_samples ),
-                                                                  lambda: [ x_pri, x_post, P_pri, P_post, self._kf_ctr ],
-                                                                  lambda: [ self._x0, self._x0, self._P0, self._P0, tf.constant(0) ])
+        x_pri, x_post, z_pri, z_post, P_pri, P_post, self._kf_ctr = tf.cond( tf.less( self._kf_ctr, self._n_samples ),
+                                                                             lambda: [ x_pri, x_post, z_pri, z_post, P_pri, P_post, self._kf_ctr ],
+                                                                             lambda: [ self._x0, self._x0, self._z0, self._z0, self._P0, self._P0, tf.constant(0) ])
 
-        #z = tf.expand_dims( z, axis = -1 )
-                
         # Predict
         x_pri = tf.matmul( self._F, x_post, name='x_pri' )
         P_pri = tf.add( tf.matmul( self._F, tf.matmul( P_post, self._F, transpose_b=True ) ), self._Q, name='P_pri' )
@@ -76,7 +77,11 @@ class KalmanFilter(AbstractFilter):
         x_post = tf.add( x_pri, tf.matmul( K, innov_plus ), name = 'x_post' )
         P_post = tf.matmul( tf.subtract( tf.eye( tf.shape( P_pri )[0], dtype=tf.float64 ), tf.matmul( K, self._H ) ), P_pri, name = 'P_post' )
 
-        return [ x_pri, x_post, P_pri, P_post, tf.add(self._kf_ctr,1) ]
+        # map state estimates to measurement space
+        z_pri  = tf.matmul(self._H, x_pri, name='z_pri', transpose_b=False)
+        z_post = tf.matmul(self._H, x_post, name='z_post', transpose_b=False)
+        
+        return [ x_pri, x_post, z_pri, z_post, P_pri, P_post, tf.add(self._kf_ctr,1) ]
 
 ###############################################################################
 
