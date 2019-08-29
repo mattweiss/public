6691bb006ab05e4c7d8654619141240440dd6f7b
Matt <mlweiss@wpi.edu>
2019-08-29 02:12:44 -0400

    1) Initial commit of change from Keras to Tensorflow 2) Things are working except for the AEKF with DR.  Suspect the Kalman Filter code

--- datasets/mnist.py
+++ datasets/mnist.py
@@ -27,19 +27,43 @@ class MNISTDataset(AbstractDataset):
         (self._dataset_dict['x_train'], self._dataset_dict['y_train']), (self._dataset_dict['x_test'], self._dataset_dict['y_test']) = tf.keras.datasets.mnist.load_data()
 
         # preprocess
-        self._dataset_dict['x_train'] = self._dataset_dict['x_train'].reshape(60000, 784).astype('float32') / 255
-        self._dataset_dict['x_test'] = self._dataset_dict['x_test'].reshape(10000, 784).astype('float32') / 255
+        self._dataset_dict['x_train'] = self._dataset_dict['x_train'].reshape(60000, 784).astype('float64') / 255
+        self._dataset_dict['x_test'] = self._dataset_dict['x_test'].reshape(10000, 784).astype('float64') / 255
 
-        # labels
-        self._dataset_dict['y_train'] = self._dataset_dict['y_train'].astype('float32')
-        self._dataset_dict['y_test'] = self._dataset_dict['y_test'].astype('float32')
+        if self._supervised:
+            
+            # supervised
+            self._dataset_dict['y_train'] = self._dataset_dict['y_train'].astype('float64')
+            self._dataset_dict['y_test'] = self._dataset_dict['y_test'].astype('float64')
 
+        else:
+
+            # unsupervised
+            self._dataset_dict['y_train'] = self._dataset_dict['x_train']
+            self._dataset_dict['y_test'] = self._dataset_dict['x_test']
+        
         if self._with_val:
         
             # Reserve 10,000 samples for validation
-            self._dataset_dict['x_val'] = self._dataset_dict['x_train'][-10000:]
-            self._dataset_dict['y_val'] = self._dataset_dict['y_train'][-10000:]
             self._dataset_dict['x_train'] = self._dataset_dict['x_train'][:-10000]
-            self._dataset_dict['y_train'] = self._dataset_dict['y_train'][:-10000]
+            self._dataset_dict['x_val'] = self._dataset_dict['x_train'][-10000:]
 
+            # supervised
+            if self._supervised:
+            
+                self._dataset_dict['y_train'] = self._dataset_dict['y_train'][:-10000]
+                self._dataset_dict['y_val'] = self._dataset_dict['y_train'][-10000:]
+
+            # unsupervised
+            else:
+
+                self._dataset_dict['y_train'] = self._dataset_dict['x_train']
+                self._dataset_dict['y_val'] = self._dataset_dict['x_val']           
+            
+        if self._onehot:
+
+            self._dataset_dict['y_train'] = tf.keras.utils.to_categorical(self._dataset_dict['y_train'], 10)
+            self._dataset_dict['y_val'] = tf.keras.utils.to_categorical(self._dataset_dict['y_val'], 10)
+            self._dataset_dict['y_test'] = tf.keras.utils.to_categorical(self._dataset_dict['y_test'], 10)
+            
         return self._dataset_dict
